539eba1018b380e14a2f995aea716a196dde5ea0
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-08-16 08:07:59 -0300

    React Fragment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 
-import Aleatorio from './components/Aleatorio'
+import Titulo from './components/Titulo'
+// import Aleatorio from './components/Aleatorio'
 // import MinMax from './components/MinMax'
 // import CompPadrao, { Comp1, Comp2 } from './components/Multi'
 // import Primeiro from './components/Primeiro'
@@ -9,13 +10,16 @@ import Aleatorio from './components/Aleatorio'
 
 export default () => (
     <View style={style.App}>
+        <Titulo principal="Cadastro Produto"
+            secundario="Tela de Cadastro do Produto" />
+        {/*
         <Aleatorio min={1} max={60} />
         <Aleatorio min={1} max={60} />
         <Aleatorio min={1} max={60} />
         <Aleatorio min={1} max={60} />
         <Aleatorio min={1} max={60} />
         <Aleatorio min={1} max={60} />
-        {/* <MinMax min={3} max={20} />
+        <MinMax min={3} max={20} />
         <MinMax min={1} max={94} />
         <CompPadrao />
         <Comp1 />
