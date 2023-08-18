4ec1b96ab228cf70bf15d46bc286ffaad9a45912
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-08-17 23:55:32 -0300

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { SafeAreaView, StyleSheet } from 'react-native'
 
-import Familia from './components/relacao/Familia'
-import Membro from './components/relacao/Membro'
+import UsuarioLogado from './components/UsuarioLogado'
+// import Familia from './components/relacao/Familia'
+// import Membro from './components/relacao/Membro'
 // import ParImpar from './components/ParImpar'
 // import Diferenciar from './components/Diferenciar'
 // import ContadorV2 from './components/contador/ContadorV2'
@@ -19,6 +20,12 @@ import Membro from './components/relacao/Membro'
 
 export default () => (
     <SafeAreaView style={style.App}>
+        <UsuarioLogado usuario={{ nome:"Gui", email:"gui@gui.com" }} />
+        <UsuarioLogado usuario={{ nome:"Ana" }} />
+        <UsuarioLogado usuario={{ email:"carlos@empresa.com" }} />
+        <UsuarioLogado usuario={null} />
+        <UsuarioLogado usuario={{}} />
+        {/*
         <Familia>
             <Membro nome="Bia" sobrenome="Arruda" />
             <Membro nome="Carlos" sobrenome="Arruda" />
@@ -29,7 +36,6 @@ export default () => (
             <Membro nome="Gui" sobrenome="Silva" />
             <Membro nome="Rebeca" sobrenome="Silva" />
         </Familia>
-        {/*
         <ParImpar num={3} />
         <Diferenciar />
         <ContadorV2 />
