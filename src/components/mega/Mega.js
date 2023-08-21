dcb195879d40e320049a6ad7ca4fd9f79de89c90
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-08-20 23:06:15 -0300

    Mega-Sena Component #02, 03

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react"
-import { Button, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, Button } from 'react-native'
 import Estilo from '../estilo'
+
+import MegaNumero from './MegaNumero'
 
 export default class Mega extends Component {
 
@@ -26,6 +28,13 @@ export default class Mega extends Component {
         this.setState({ numeros })
     }
 
+    exibirNumeros = () => {
+        const nums = this.state.numeros
+        return nums.map(num => {
+            return <MegaNumero key={num} num={num} />
+        })
+    }
+
     render() {
         return (
             <>
@@ -43,9 +52,14 @@ export default class Mega extends Component {
                     title='Gerar'
                     onPress={this.gerarNumeros}
                 />
-                <Text>
-                    {this.state.numeros.join(',')}
-                </Text>
+                <View style={{
+                    marginTop: 20,
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    justifyContent: 'center'
+                }}>
+                    {this.exibirNumeros()}
+                </View>
             </>
         )
     }
