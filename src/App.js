8bd866e8e824a4fea2f6ec79fb9ce6d189065be6
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-08-20 23:04:21 -0300

    Mega-Sena Component #01

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ import Mega from './components/mega/Mega'
 
 export default () => (
     <SafeAreaView style={style.App}>
-        <Mega />
+        <Mega qtdeNumeros={12} />
         {/*
         <FlexboxV4 />
         <FlexboxV3 />
